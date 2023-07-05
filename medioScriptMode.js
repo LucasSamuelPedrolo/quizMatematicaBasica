74e76f76f6807ff4652cf6238e96e9986d09add6
Lucas Pedrolo <132529241+LucasSamuelPedrolo@users.noreply.github.com>
2023-07-04 23:26:20 -0300

    adicionando js

---
+++ medioScriptMode.js
@@ -0,0 +1,121 @@
+const questao = document.getElementById('question');
+const respostas = document.querySelectorAll('#btn-question');
+const certaResposta = document.getElementById('result');
+let operacao = geradorQuestao();
+const timer = document.getElementById('timer');
+const pontuacao = document.getElementById('pontuacao');
+
+const segundos = 1;
+let timerStart;
+let displayTempo = 7;
+let pontos = 0;
+
+questao.innerHTML = `Qual o resultado dessa questão? ${operacao}`;
+
+inicializadorContagem(segundos);
+
+btnCorrect();
+
+for (const botao of respostas) {
+    if (!botao.innerHTML) {
+        botao.innerHTML = criaFalsaRespostas(Number(eval(operacao)));
+    }
+    botao.addEventListener('click', (e) => {
+        const el = e.target;
+        verificaJogada(el.innerHTML, operacao);
+    })
+}
+
+function novoJogo() {
+    for (const botao of respostas) {
+        if (!botao.innerHTML) {
+            botao.innerHTML = Number(criaFalsaRespostas(Number(eval(operacao))));
+        }
+    }
+
+    displayTempo = 7;
+}
+
+function verificaJogada(escolha1, RespCorreta) {
+    if (Number(escolha1) === Number(eval(RespCorreta))) {
+        certaResposta.innerHTML = "";
+        certaResposta.innerHTML = "Anterior : Certa resposta +1 ponto";
+        certaResposta.style.color = 'green'
+        pontuacaoUser(+1);
+        operacao = geradorQuestao();
+        btnCorrect();
+        novoJogo();
+        questao.innerHTML = `Qual o resultado dessa questão? ${operacao}`;
+    } else {
+        certaResposta.innerHTML = "";
+        certaResposta.innerHTML = `Anterior : Resposta Errada -1 Correta anterior: ${eval(operacao)}`;
+        pontuacaoUser(-1);
+        certaResposta.style.color = 'red';
+        operacao = geradorQuestao();
+        btnCorrect();
+        novoJogo();
+        questao.innerHTML = `Qual o resultado dessa questão? ${operacao}`;
+    }
+
+    displayTempo = 7;
+
+}
+
+function btnCorrect() {
+    for (const botao of respostas) {
+        botao.innerHTML = "";
+    }
+    const botaoEscolhido = respostas[Math.floor(Math.random() * respostas.length)];
+    botaoEscolhido.innerHTML = Number(eval(operacao));
+}
+
+function geradorQuestao() {
+    const ope = ['+', '-', '*', '/'];
+    const num1 = Math.floor(Math.random() * (20 - 1) + 1);
+    const num2 = Math.floor(Math.random() * (20 - 1) + 1);
+    const opechoice = ope[Math.floor(Math.random() * ope.length)];
+    console.log(num1, num2, opechoice);
+    return `${num1} ${opechoice} ${num2}`;
+}
+
+function criaFalsaRespostas(resultadoOriginal) {
+    const resultadoFalso = resultadoOriginal - Math.floor(Math.random() * (7 - 1) + 1);
+    return resultadoFalso;
+}
+
+function inicializadorContagem(seg) {
+    const segundos = seg * 1000;
+
+    timerStart = setInterval(() => {
+        timer.innerHTML = --displayTempo;
+
+        if (displayTempo === 0) {
+            certaResposta.innerHTML = `Anterior : Tempo esgotado -1 pontos, Correta anterior : ${eval(operacao)} `;
+            pontuacaoUser(-1);
+            certaResposta.style.color = 'red';
+            displayTempo = 0;
+            operacao = geradorQuestao();
+            questao.innerHTML = `Qual o resultado dessa questão? ${operacao}`;
+            btnCorrect();
+            novoJogo();
+        }
+    }, segundos);
+
+}
+
+function pontuacaoUser(valor) {
+    if (valor < 0) {
+        console.log('passou negativo')
+        --pontos;
+    }
+    if (valor > 0) {
+        console.log('passou positivo')
+        ++pontos;
+    }
+    if (pontos < 0) {
+        console.log('zerou');
+    }
+
+    pontuacao.innerHTML = `pontuacao : ${pontos} pontos`;
+
+}
